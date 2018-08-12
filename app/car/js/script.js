d84f3a01a6c101512cc6dfc9032a26a44c7ca5ac
Suren Avetisyan <surenavetisyan24@gmail.com>
2018-08-12 22:49:54 +0400

    Lesson-10

---
+++ app/car/js/script.js
@@ -0,0 +1,9 @@
+// 'use strict';
+// var app = angular.module('myApp', []);
+// app.controller('myCtrl', function($scope, $http) {
+//     $http.get("car/cars.json")
+//         .then(function(response) {
+//             $scope.myWelcome = response.data;
+//             console.log(response.data);
+//         });
+// });
